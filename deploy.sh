5965f2d0d540cbe59d71a670ee3f8b299ced396e
MarcosReyero <reyeromateo@gmail.com>
2026-05-14 13:53:31 -0300

    feat: agregar opción --force-recreate al script de despliegue para asegurar la recreación de contenedores

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -e
 
 git pull
-docker compose -p worm_erp --env-file .env -f deploy/docker-compose.prod.yml up -d --build
+docker compose -p worm_erp --env-file .env -f deploy/docker-compose.prod.yml up -d --build --force-recreate
